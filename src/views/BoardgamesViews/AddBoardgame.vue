60c72f5ed4b9f4ebd809338db1f9eba38c7ac920
pecarcer <pecarcer@gmail.com>
2021-05-26 07:21:11 -0700

    unfinished crud boardgame

---
+++ src/views/BoardgamesViews/AddBoardgame.vue
@@ -0,0 +1,86 @@
+<template>
+  <form>
+    <p class="h4 text-center mb-4">Añadir Juego</p>
+    <br/>
+
+    <div class="grey-text">
+      <div class="four wide field">
+        <input type="text" v-model="title" placeholder="Título" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="creator" placeholder="Creadores" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="release" placeholder="Fecha de lanzamiento" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="min_num_players" placeholder="Número mínimo de jugadores" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="max_num_players" placeholder="Número Máximo de Jugadores" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="playing_time" placeholder="Duración de partida" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="ages" placeholder="Edad mínima" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="publisher" placeholder="Editorial" />
+      </div>
+      <div class="four wide field">
+        <input type="text" v-model="image" placeholder="Imagen" />
+      </div>
+    </div>
+    <br />
+    <div class="text-center">
+      <button color="primary" @click="saveBoardgame()">Añadir</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AddBoardgame",
+
+  data() {
+    return {
+      title: "",
+      creator: "",
+      release: "",
+      min_num_players: "",
+      max_num_players: "",
+      playing_time: "",
+      ages: "",
+      publisher: "",
+      image: "",
+    };
+  },
+  methods: {
+    saveBoardgame() {
+      let self = this;
+      axios
+        .post("/boardgames/add", {
+          title: this.title,
+          creator: this.creator,
+          release: this.release,
+          min_num_players: this.min_num_players,
+          max_num_players: this.max_num_players,
+          playing_time: this.playing_time,
+          ages: this.ages,
+          publisher: this.publisher,
+          image: this.image,
+        })
+        .then(self.$router.push("/boardgames"))
+        .catch((e) => {
+          alert(e);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/BoardgamesViews/Boardgames.vue
+++ src/views/BoardgamesViews/Boardgames.vue
@@ -1,5 +1,105 @@
 <template>
-  <div class="about">
-    <h1>Aquí los juegos de mesa</h1>
+  <div>
+    <h4 class="text-center">Juegos de Mesa</h4>
+    <br />
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Creador</th>
+          <th>Publicación</th>
+          <th>Jugadores Min.</th>
+          <th>Jugadores Max</th>
+          <th>Duración</th>
+          <th>Edad Mín.</th>
+          <th>Editorial</th>
+          <th>Imagen</th>
+          <th>Acciones</th>
+
+
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="boardgame in boardgames" :key="boardgame.id">
+          <td>{{ boardgame.id }}</td>
+          <td>{{ boardgame.title }}</td>
+          <td>{{ boardgame.creator }}</td>
+          <td>{{ boardgame.release }}</td>
+          <td>{{ boardgame.min_num_players }}</td>
+          <td>{{ boardgame.max_num_players}}</td>
+          <td>{{ boardgame.playing_time }}</td>
+          <td>{{ boardgame.ages }}</td>
+          <td>{{ boardgame.publisher }}</td>
+          <td>{{ boardgame.image }}</td>
+
+          <td>
+            <div class="btn-group" role="group">
+              <button class="btn btn-primary" @click="editBoardgame(boardgame.id)">
+                Editar
+              </button>
+              <button class="btn btn-danger" @click="confirmarDelete(boardgame.id)">
+                Borrar
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <button type="button" class="btn btn-info">
+      <router-link to="/addboardgame">Añadir Juego</router-link>
+    </button>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      boardgames: [],
+      url: "http://127.0.0.1:8000/api/boardgames",
+    };
+  },
+  methods: {
+    getBoardgames() {
+      axios.get(this.url).then((data) => {
+        this.boardgames = data.data;
+      });
+    },
+    editBoardgame(idToEdit) {
+      this.$router.push('/boardgames/edit/'+idToEdit)
+    },
+
+    deleteBoardgame(idToDelete) {
+      axios
+        .delete("/boardgames/delete/" + idToDelete)
+        .then(() => {
+          this.getBoardgames();
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    },
+    confirmarDelete(idToDelete) {
+      this.$confirm({
+        message: "Are you sure?",
+        button: {
+          no: "No",
+          yes: "Yes",
+        },
+        callback: (confirm) => {
+          if (confirm) {
+            this.deleteBoardgame(idToDelete);
+          }
+        },
+      });
+    },
+  },
+  created() {
+    this.getBoardgames();
+  },
+};
+</script>
