0f3f1655f0113b88bdd696c7c1b6048c8e2822e8
José Carmona Cervera <38842510+Pecarcer@users.noreply.github.com>
2021-06-10 16:58:38 +0200

    mejorado tablas

--- src/views/UserViews/Users.vue
+++ src/views/UserViews/Users.vue
@@ -1,32 +1,53 @@
 <template>
   <div>
-    <h4 class="text-center">Usuarios</h4>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Usuarios</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- poner avatar y nombre-->
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <br />
     <b-container>
       <b-col></b-col>
       <b-col>
-        <b-table-simple striped hover borderless class="table table-bordered">
-          <thead>
-            <tr>
-              <th class="leftth">ID</th>
-              <th>Nick</th>
-              <th>Email</th>
-              <th>Rol</th>
-              <th>Full name</th>
-              <th>Avatar</th>
-              <th class="rightth">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="user in users" :key="user.id">
-              <td>{{ user.id }}</td>
-              <td>{{ user.nick }}</td>
-              <td>{{ user.email }}</td>
-              <td>{{ user.role }}</td>
-              <td>{{ user.fullname }}</td>
-              <td>{{ user.avatar }}</td>
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          aria-controls="my-table"
+        ></b-pagination>
 
-              <td>
+        <b-table
+          striped
+          hover
+          borderless
+          fixed
+          :per-page="perPage"
+          :current-page="currentPage"
+          :items="items"
+          :fields="fields"
+          class="table table-bordered"
+        >
+        
+
+          <template #cell(acciones)="data">
+            <div class="btn-group" role="group">
+              <button class="btn btn-primary" @click="editUser(data.id)">
+                Editar
+              </button>
+              <button class="btn btn-danger" @click="confirmarDelete(data.id)">
+                Borrar
+              </button>
+            </div>
+          </template>
+
+          <!--<td>
                 <div class="btn-group" role="group">
                   <button class="btn btn-primary" @click="editUser(user.id)">
                     Editar
@@ -40,7 +61,8 @@
                 </div>
               </td>
             </tr>
-            <tr class="tablefooter">	
+
+            <tr class="tablefooter">
               <th class="bottomleft"></th>
               <th></th>
               <th></th>
@@ -48,16 +70,15 @@
               <th></th>
               <th></th>
               <th class="bottomright"></th>
-            </tr>
-          </tbody>
-        </b-table-simple>
+            </tr>-->
+        </b-table>
       </b-col>
-      <b-col></b-col>
+      <b-col> </b-col>
     </b-container>
 
-    <button type="button" class="btn btn-info">
-      <router-link to="/adduser">Añadir Usuario</router-link>
-    </button>
+    <b-button type="button" to="/adduser" class="newbtn">
+      Añadir Usuario
+    </b-button>
   </div>
 </template>
 
@@ -67,14 +88,50 @@ import axios from "axios";
 export default {
   data() {
     return {
-      users: [],
-      url: "http://127.0.0.1:8000/api/users"
+      items: [],
+      url: "http://127.0.0.1:8000/api/users",
+      perPage: 5,
+      currentPage: 1,
+      fields: [
+        {
+          key: "id",
+          sortable: true,
+        },
+        {
+          key: "nick",
+          sortable: true,
+          label: "Nick",
+        },
+        {
+          key: "email",
+          sortable: true,
+        },
+        {
+          key: "role",
+          sortable: true,
+          label: "Rol",
+        },
+        {
+          key: "fullname",
+          sortable: true,
+          label: "Nombre Completo",
+        },
+        {
+          key: "avatar",
+          sortable: false,
+        },
+        {
+          key: "acciones",
+          sortable: false,
+          label: "Acciones",
+        },
+      ],
     };
   },
   methods: {
     getUsers() {
       axios.get(this.url).then((data) => {
-        this.users = data.data;
+        this.items = data.data;
       });
     },
     editUser(idToEdit) {
@@ -93,10 +150,10 @@ export default {
     },
     confirmarDelete(idToDelete) {
       this.$confirm({
-        message: "Are you sure?",
+        message: "¿Estás seguro?",
         button: {
           no: "No",
-          yes: "Yes",
+          yes: "Sí",
         },
         callback: (confirm) => {
           if (confirm) {
@@ -109,32 +166,42 @@ export default {
   created() {
     this.getUsers();
   },
+  computed: {
+    rows() {
+      return this.items.length;
+    },
+  },
 };
 </script>
 
 <style scoped>
-th,.tablefooter{
+th,
+.tablefooter, .newbtn {
   text-align: center;
   color: white;
   background: green;
 }
-.rightth{
+.rightth {
   border-top-right-radius: 25px;
 }
-.leftth{
-  border-top-left-radius:25px;
+.leftth {
+  border-top-left-radius: 25px;
 }
-.bottomleft{
+.bottomleft {
   border-bottom-left-radius: 25px;
 }
-.bottomright{
+.bottomright {
   border-bottom-right-radius: 25px;
+}
+col {
+  border: none;
+}
+table {
+  border: none;
+}
 
-}
-col{
-  border: none;
-}
-table{
-  border: none;
+.navbar.navbar-dark.bg-dark {
+  background-color: green !important;
+  border-radius: 25px;
 }
 </style>
